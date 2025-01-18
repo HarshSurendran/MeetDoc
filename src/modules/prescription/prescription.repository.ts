ab5f382b91ef66cffe5f0f45d125802a58b5c1c1
Harsh Surendran <harshsurendran@gmail.com>
2025-01-18 11:55:15 +0530

    ADD - Implement PDF generation for prescriptions; create CreatePrescriptionPdfDto for prescription data structure; enhance PrescriptionService to generate and save prescription PDFs locally.

--- src/modules/prescription/prescription.repository.ts
+++ src/modules/prescription/prescription.repository.ts
@@ -3,16 +3,37 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Prescription, PrescriptionDocument } from "./prescription.entity";
 import { CreatePrescriptionDto } from "./dto/create-prescription.dto";
-
+import { PrescriptionService } from "./prescription.service";
+import * as moment from 'moment';
+import { CreatePrescriptionPdfDto } from "./dto/create-prescriptionpdf.dto";
 
 
 @Injectable()
 export class PrescriptionRepository {
-    constructor(@InjectModel('Prescription') private PrescriptionModel: Model<PrescriptionDocument>) { }
+    constructor(
+        @InjectModel('Prescription') private PrescriptionModel: Model<PrescriptionDocument>,
+        private prescriptionService: PrescriptionService
+    ) { }
+
+    
+
+    calculateAge(dob: Date): number {
+        try {
+            
+            return moment().diff(moment(dob, "YYYY-MM-DD"), 'years');
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     async createPrescription(prescriptionDto: CreatePrescriptionDto): Promise<Prescription> {
       const prescription = new this.PrescriptionModel(prescriptionDto);
-      return prescription.save();
+        const result = await prescription.save();
+        const detailedPrescription = await this.PrescriptionModel.findById(result._id).populate('patientId', 'name gender date_of_birth').populate('doctorId', 'name specialistation').exec() as unknown as CreatePrescriptionPdfDto;
+        const patientAge = this.calculateAge(detailedPrescription.patientId.date_of_birth);
+        detailedPrescription.patientId.age = patientAge;
+        const pdfUrl = await this.prescriptionService.generatePrescriptionPDF(detailedPrescription);
+        return result;
     }
 
     async getPrescriptionsByPatientId(patientId: string): Promise<Prescription[]> {
--- src/modules/prescription/prescription.service.ts
+++ src/modules/prescription/prescription.service.ts
@@ -1,4 +1,139 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { PrescriptionDocument } from './prescription.entity';
+import * as fs from 'fs';
+import * as PDFDocument from 'pdfkit';
 
 @Injectable()
-export class PrescriptionService {}
+export class PrescriptionService {
+    constructor(@InjectModel('Prescription') private PrescriptionModel: Model<PrescriptionDocument>) { }
+
+
+    async generatePrescriptionPDF(prescription): Promise<string> {
+        try {
+            const doc = new PDFDocument({ margin: 50 });
+            const fileName = `prescription_${prescription._id}.pdf`;
+            const filePath = `./uploads/${fileName}`;
+            doc.pipe(fs.createWriteStream(filePath));
+
+            //change the design of the pdf later        
+            // Header Section
+            doc
+                .fontSize(24)
+                .fillColor('#1976D2')
+                .text('MeetDoc', { align: 'center', underline: true });
+        
+            doc.moveDown();
+            doc
+                .fontSize(12)
+                .fillColor('black')
+                .text(`Prescription ID: ${prescription._id}`, { align: 'right' })
+                .text(`Date: ${new Date().toDateString()}`, { align: 'right' });
+        
+            doc.moveDown(2);
+            
+            // Doctor and Patient Details
+            doc
+                .fontSize(16)
+                .fillColor('#333')
+                .text('Doctor Details:', { underline: true })
+                .moveDown(0.5)
+                .fontSize(12)
+                .text(`Name: Dr. ${prescription.doctorId.name}`)
+                .text(`Specialization: ${prescription.doctorId.specialisation}`)
+                // .text(`Reg. No: ${prescription.doctorId.registrationNumber}`)
+                .moveDown();
+        
+            doc
+                .fontSize(16)
+                .fillColor('#333')
+                .text('Patient Details:', { underline: true })
+                .moveDown(0.5)
+                .fontSize(12)
+                .text(`Name: ${prescription.patientId.name}`)
+                .text(`Age: ${prescription.patientId.age}`)
+                .text(`Gender: ${prescription.patientId.gender}`)
+                .moveDown();
+        
+            // Medications Table
+            doc
+                .fontSize(16)
+                .fillColor('#1976D2')
+                .text('Medications', { underline: true })
+                .moveDown(0.5);
+        
+            prescription.medications.forEach((med, index) => {
+                doc
+                    .fontSize(12)
+                    .fillColor('black')
+                    .text(
+                        `${index + 1}. ${med.medicationName} - ${med.dosage} (${med.frequency}), Qty: ${med.quantity}`
+                    );
+            });
+        
+            doc.moveDown(1);
+        
+            // Additional Notes
+            if (prescription.additionalNotes) {
+                doc
+                    .fontSize(14)
+                    .fillColor('#1976D2')
+                    .text('Additional Notes:', { underline: true })
+                    .moveDown(0.5)
+                    .fontSize(12)
+                    .fillColor('black')
+                    .text(prescription.additionalNotes);
+                doc.moveDown(1);
+            }
+        
+            // Follow-Up Date
+            doc
+                .fontSize(12)
+                .fillColor('#333')
+                .text(
+                    `Follow-up Date: ${prescription.followUpDate
+                        ? prescription.followUpDate.toDateString()
+                        : 'Not specified'
+                    }`
+                );
+        
+            doc.moveDown(2);
+        
+            // Footer with Doctor's Signature
+            doc
+                .fontSize(12)
+                .fillColor('#1976D2')
+                .text('Doctor’s Signature:', { underline: true })
+                .moveDown(1)
+                // .image('./uploads/signature.png', {
+                //     width: 100,
+                //     align: 'left',
+                // });
+        
+            doc.moveDown(2);
+        
+            // Footer with Platform Info
+            doc
+                .fontSize(10)
+                .fillColor('#666')
+                .text(
+                    'This prescription is digitally generated and is valid without a physical signature.',
+                    { align: 'center' }
+                )
+                .moveDown(0.5)
+                .text('MeetDoc | www.meetdoc.com | support@meetdoc.com', {
+                    align: 'center',
+                });
+        
+            doc.end();
+            return filePath;
+            
+        
+        } catch (error) {
+            console.log(error);
+            throw new InternalServerErrorException("Could not generate pdf. Please try again later.");
+        }
+    };
+}
+
