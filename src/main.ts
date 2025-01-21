a0e96c05d13772b68a6dc7f89f5a7569a714fd55
Harsh Surendran <harshsurendran@gmail.com>
2025-01-21 11:05:24 +0530

    REFINE - Enhance WebRTC gateway methods to include client ID in emitted messages; add join-room and negotiation message handling for improved communication flow.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,10 @@ import * as express from 'express';
 import { TransformResponseInterceptor } from './common/interceptors/transform-response.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
-import bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    bodyParser: false
+    bodyParser: false,
   });
   app.enableCors({
     origin: "http://localhost:5173",
@@ -23,8 +22,8 @@ async function bootstrap() {
       next();
     }
   );
-  app.use(express.json({ limit: '50mb' })); // Example: Increase JSON payload limit
-  app.use(express.urlencoded({ extended: true, limit: '50mb' })); // Example: Increase URL-encoded payload limit
+  app.use(express.json({ limit: '50mb' })); 
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformResponseInterceptor());
--- src/modules/webrtc/webrtc.gateway.ts
+++ src/modules/webrtc/webrtc.gateway.ts
@@ -28,21 +28,42 @@ export class WebrtcGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   }
 
   @SubscribeMessage('offer')
-  handleOffer(client: Socket, payload: any): void { // Payload: { offer, target }
-    console.log(`Offer received from ${client.id} for ${payload.target}`);
-    this.server.to(payload.target).emit('offer', payload.offer); // Send offer to target
+  handleOffer(client: Socket, payload: any): void {
+    console.log(`Offer received from ${client.id} for ${payload.target} , ${payload.offer}`);    
+    this.server.to(payload.target).emit('offer', { target: client.id, offer: payload.offer }); 
+  }
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(client: Socket, payload: any): void { 
+    console.log(`Client ${client.id} joined room ${payload.roomId}`);
+    this.server.to(client.id).emit('join-room', {payload});
+    this.server.to(payload.roomId).emit('NewUserJoined', { userSocketId : client.id });
+    client.join(payload.roomId); 
+    
   }
 
   @SubscribeMessage('answer')
-  handleAnswer(client: Socket, payload: any): void { // Payload: { answer, target }
+  handleAnswer(client: Socket, payload: any): void { 
     console.log(`Answer received from ${client.id} for ${payload.target}`);
-    this.server.to(payload.target).emit('answer', payload.answer); // Send answer to target
+    this.server.to(payload.target).emit('answer', { target: client.id, answer: payload.answer });
   }
 
   @SubscribeMessage('ice-candidate')
-  handleIceCandidate(client: Socket, payload: any): void { // Payload: { candidate, target }
+  handleIceCandidate(client: Socket, payload: any): void {
     console.log(`ICE candidate received from ${client.id} for ${payload.target}`);
-    this.server.to(payload.target).emit('ice-candidate', payload.candidate); // Send candidate to target
+    this.server.to(payload.target).emit('ice-candidate', payload.candidate); 
+  }
+
+  @SubscribeMessage('negotiation-offer')
+  handleNegotiationOffer(client: Socket, payload: any): void { 
+    console.log(`Negotiation offer received from ${client.id} for ${payload.target}`);    
+    this.server.to(payload.target).emit('negotiation-offer', { target: client.id, offer: payload.offer }); 
+  }
+
+  @SubscribeMessage('negotiation-answer')
+  handleNegotitationAnswer(client: Socket, payload: any): void{
+    console.log(`Negotiation answer received from ${client.id} for ${payload.target}`); 
+    this.server.to(payload.target).emit('negotiation-answer', { target: client.id, answer: payload.answer });
   }
 }
 
