2a69a17f78deb5889dcbfc187c1f2c5f26f5e922
Harsh Surendran <harshsurendran@gmail.com>
2025-01-24 18:32:16 +0530

    REFINE - Implement review service and controller; add endpoints for creating and fetching reviews.

---
+++ src/modules/review/review.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "../auth/decorators/current-user.decorator";
+import { CreateReviewDto } from "./dto/create-review.dto";
+import { ReviewService } from "./review.service";
+
+
+@Controller('review')
+export class ReviewController {
+    constructor(private reviewService: ReviewService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('/')
+    async createReview(@CurrentUser('userId') userId: string, @Body() createReviewDto: CreateReviewDto) {
+        return await this.reviewService.createReview(createReviewDto);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('/:doctorId')
+    async getReviews(@CurrentUser('userId') userId: string, @Param('doctorId') doctorId: string) {
+        return await this.reviewService.getReviews(doctorId)
+    }
+}
--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ReviewRepository } from './review.repository';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+    constructor(@Inject() private reviewRepo: ReviewRepository){}
+
+    async createReview(createReviewDto: CreateReviewDto) {
+        const review = await this.reviewRepo.createReview(createReviewDto);
+        return {
+            review
+        }
+    }
+
+    async getReviews(doctorId: string) {
+        return await this.reviewRepo.getReviews(doctorId);
+    }
+
+    async getSingleReview(reviewId: string) {
+        return await this.reviewRepo.getReview(reviewId);
+    }
+
+
+
+}
