228a4473146cea911813c85fcf927ead7e7b16dc
Harsh Surendran <harshsurendran@gmail.com>
2025-02-14 22:47:11 +0530

    REFINE - Update CORS configuration to allow requests from the new production domain

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     bodyParser: false,
   });
   app.enableCors({
-    origin: "http://localhost:5173",
+    origin: ['https://meetdoc.harshsurendran.online'],
     credentials: true
   });
   app.use(
