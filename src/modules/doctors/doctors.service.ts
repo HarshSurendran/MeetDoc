6ab984fc72827cd5064b552de717f67a1c4b7917
Harsh Surendran <harshsurendran@gmail.com>
2025-02-19 19:46:34 +0530

    REFINE - Replace moment with moment-timezone in DoctorsService; adjust appointment date and time formatting to use timezone-specific formatting.

--- src/modules/doctors/doctors.service.ts
+++ src/modules/doctors/doctors.service.ts
@@ -10,7 +10,7 @@ import { SlotsRepository } from '../slots/slots.repository';
 import { GenerateSlotDto } from '../slots/dto/create-slot.dto';
 import { BookingsRepository } from '../bookings/bookings.repository';
 import { IBookedAppointmentType } from '../bookings/dto/doctor-booking.dto';
-import * as moment from 'moment';
+import * as moment from 'moment-timezone';
 import { PrescriptionRepository } from '../prescription/prescription.repository';
 import { CreatePrescriptionDto } from '../prescription/dto/create-prescription.dto';
 import { UsersRepository } from '../users/users.repository';
@@ -176,9 +176,9 @@ export class DoctorsService {
           patientName: appointment.patientName,
           doctorName: appointment.doctorName,
           appointmentForName: appointment.appointmentForName,
-          bookingTime: moment(appointment.bookingTime).format('DD-MM-YYYY hh:mm A'),
-          date: moment(appointment.date).format('DD-MM-YYYY'),
-          time: moment(appointment.time).format('hh:mm A'),
+          bookingTime: moment(appointment.bookingTime).tz('Asia/Kolkata').format('DD-MM-YYYY hh:mm A'),
+          date: moment(appointment.date).tz('Asia/Kolkata').format('DD-MM-YYYY'),
+          time: moment(appointment.time).tz('Asia/Kolkata').format('hh:mm A'),
         })
       }
     })
